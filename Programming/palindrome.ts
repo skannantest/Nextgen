a3eb5f033e64760cd5949fc00cd1bcd57c984274
skannantest <skannan2002k@gmail.com>
2026-08-18 16:32:45 +0530

    palindrome code updated

--- Programming/palindrome.ts
+++ Programming/palindrome.ts
@@ -1,17 +1,25 @@
+import * as readline from 'readline';
+
 function triangle(){
 
-    let palindrom = "kannan"
-    let reverse="";
+    const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+    rl.question("Enter the string to check palindrome: ", (answer) => {
+        
+        const palindrom = answer ?? "";
+        let reverse = "";
 
-    for(let i =palindrom.length-1;i>=0;i--){
-        reverse = reverse + palindrom.charAt(i);
-    }
-    
-    if (palindrom == reverse ){
-        console.log(reverse+ " Is the palindrome")
-    }
-    else{
-        console.log(reverse+ "Is not the palindrome")
-    }
+        for (let i = palindrom.length - 1; i >= 0; i--) {
+            reverse += palindrom.charAt(i);
+        }
+
+        if (palindrom === reverse) {
+            console.log(palindrom + " is a palindrome");
+        } else {
+            console.log(palindrom + " is not a palindrome");
+        }
+
+        rl.close();
+    });
 }
+
 triangle();
